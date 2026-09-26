fe00a91fafa9c296060919b4204a9714e6d8e9dd
Felipeliberatosouza <160543583+Felipeliberatosouza@users.noreply.github.com>
2026-09-25 21:14:39 -0300

    Alteração via automação: No cabeçalho do site revisaofacil.com, altere o texto do bot

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -528,7 +528,7 @@ const Navbar = () => {
           ) : (
             <Link to="/login">
               <Button size="sm" className="gap-2 font-display">
-                <User className="h-4 w-4" /> Entarar
+                <User className="h-4 w-4" /> Entrar
               </Button>
             </Link>
           )}
@@ -652,7 +652,7 @@ const Navbar = () => {
             {!user && (
               <Link to="/login" onClick={() => setMobileOpen(false)}>
                 <Button size="sm" className="h-12 w-full gap-2 font-display">
-                  <User className="h-4 w-4" /> Entarar
+                  <User className="h-4 w-4" /> Entrar
                 </Button>
               </Link>
             )}
